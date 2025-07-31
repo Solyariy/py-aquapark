8bba86eab69f585527cbca3420c8954e2582f436
Oleksandr Skoromnov <solalex005@gmail.com>
2025-07-31 06:05:14 +0300

    fix3

--- app/main.py
+++ app/main.py
@@ -3,7 +3,11 @@ from typing import Any
 
 
 class IntegerRange:
-    def __init__(self, min_amount: int, max_amount: int) -> None:
+    def __init__(
+            self,
+            min_amount: int,
+            max_amount: int
+    ) -> None:
         self.min_amount = min_amount
         self.max_amount = max_amount
 
